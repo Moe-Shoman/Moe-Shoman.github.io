53dcc6283f47dfee416612612820c437b35c1b81
Mo-Sho <muhama94@gmail.com>
2017-02-10 10:49:29 -0800

    add drop and horizontal functionality

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 const grid = [];
+let blocks = {};
+let currentBlock;
+let currentBlockName;
+let currentPosition = []
+let dropCount = 0;
+let horizontalstart = 3;
 
 function createGrid() {
     //draw lines down
@@ -21,7 +27,6 @@ function createGrid() {
 
 }
 createGrid();
-let blocks = {};
 // Creates 4 arrays of positions for each block and places it in blocks Object.
 function blockObjMaker(blockName, ...ind) {
     let positions = [];
@@ -35,6 +40,7 @@ function blockObjMaker(blockName, ...ind) {
     positions = [positions.slice(0, 4), positions.slice(4, 8), positions.slice(8, 12), positions.slice(12, 16)];
     blocks[blockName] = {
         positions: positions,
+        check: {}
     }
 }
 blockObjMaker("I", 0, 4, 8, 12);
@@ -47,10 +53,9 @@ blockObjMaker("T", 1, 4, 5, 6);
 
 function checkAround(blockName, dir, ...pos) {
     let underPositions = [];
-    blocks[blockName].check = {};
+    blocks[blockName].check[dir] = underPositions;
     for (var i = 0; i < pos.length; i++) {
         underPositions.push(pos[i]);
-        blocks[blockName].check[dir] = underPositions;
     }
 }
 checkAround("I", "down", [4, 0]);
@@ -60,6 +65,20 @@ checkAround("O", "down", [2, 0], [2, 1]);
 checkAround("Z", "down", [1, 0], [2, 1], [2, 2]);
 checkAround("S", "down", [2, 0], [2, 1], [1, 2]);
 checkAround("T", "down", [2, 0], [2, 1], [2, 2]);
+checkAround("I", "right", [0, 1], [1, 1], [2, 1], [3, 1]);
+checkAround("L", "right", [0, 1], [1, 1], [2, 2]);
+checkAround("J", "right", [0, 3], [1, 3], [2, 3]);
+checkAround("O", "right", [0, 2], [1, 2]);
+checkAround("Z", "right", [0, 2], [1, 3]);
+checkAround("S", "right", [0, 3], [1, 2]);
+checkAround("T", "right", [0, 2], [1, 3]);
+checkAround("I", "left", [0, -1], [1, -1], [2, -1], [3, -1]);
+checkAround("L", "left", [0, -1], [1, -1], [2, -1]);
+checkAround("J", "left", [0, 1], [1, 1], [2, 0]);
+checkAround("O", "left", [0, -1], [1, -1]);
+checkAround("Z", "left", [0, -1], [1, 0]);
+checkAround("S", "left", [0, 0], [1, -1]);
+checkAround("T", "left", [0, 0], [1, -1]);
 
 function gridPositions() {
     for (var i = 0; i < 20; i++) {
@@ -72,19 +91,17 @@ function pickBlock() {
     const blockNames = ["I", "L", "J", "O", "Z", "S", "T"];
     return blockNames[Math.floor(blockNames.length * Math.random())];
 }
-let currentBlock;
-let currentBlockName;
-let currentPosition = []
 
-function addBlock(block, verticalDrop) {
+function addBlock(block, verticalDrop, horizontalOffset) {
     verticalDrop = verticalDrop || 0;
+    horizontalOffset = horizontalstart;
     currentBlockName = currentBlockName || pickBlock();
     currentBlock = blocks[currentBlockName]['positions'];
     for (var i = 0; i < currentBlock.length; i++) {
         for (var j = 0; j < 4; j++) {
             if (currentBlock[i][j] === 1) {
-                currentPosition.push([i + verticalDrop, j + 3]);
-                grid[i + verticalDrop][j + 3] += 1;
+                currentPosition.push([i + verticalDrop, j + horizontalOffset]);
+                grid[i + verticalDrop][j + horizontalOffset] += 1;
             }
         }
     }
@@ -93,28 +110,70 @@ function addBlock(block, verticalDrop) {
 function checkDown() {
     let unders = blocks[currentBlockName].check.down;
     for (var i = 0; i < unders.length; i++) {
-        if (((unders[i][0] + dropCount === 20) || grid[unders[i][0] + dropCount][(unders[i][1]) + 3])) {
+        if (((unders[i][0] + dropCount === 20) || grid[unders[i][0] + dropCount][(unders[i][1]) + horizontalstart])) {
             return false;
         }
-        return true;
+    }
+    return true;
+}
+
+function checkRight() {
+    let rights = blocks[currentBlockName].check.right;
+    for (var i = 0; i < rights.length; i++) {
+        if (rights[i][1] + horizontalstart === 10 || grid[rights[i][0] + dropCount][(rights[i][1]) + horizontalstart]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function checkLeft() {
+    let lefts = blocks[currentBlockName].check.right;
+    for (var i = 0; i < lefts.length; i++) {
+        if (lefts[i][1] + horizontalstart === 1 || grid[lefts[i][0] + dropCount][(lefts[i][1] + horizontalstart)]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function clearBlock() {
+    for (var i = 0; i < currentPosition.length; i++) {
+        let row = currentPosition[i][0];
+        let cell = currentPosition[i][1];
+        grid[row][cell] = 0;
     }
 }
-let dropCount = 0;
 
 function drop() {
     if (checkDown()) {
-        for (var i = 0; i < currentPosition.length; i++) {
-            let row = currentPosition[i][0];
-            let cell = currentPosition[i][1];
-            grid[row][cell] = 0;
-        }
-        dropCount++
+        clearBlock()
+        dropCount++;
         addBlock(currentBlockName, dropCount);
     } else {
         currentPosition = [];
         dropCount = 0;
+        horizontalstart = 3;
         currentBlockName = pickBlock()
         addBlock();
+    }
+}
+
+function right() {
+    if (checkRight()) {
+        clearBlock();
+        horizontalstart++;
+        currentPosition = [];
+        addBlock(currentBlockName, dropCount, horizontalstart);
+    }
+}
+
+function left() {
+    if (checkLeft()) {
+        clearBlock();
+        horizontalstart--;
+        currentPosition = [];
+        addBlock(currentBlockName, dropCount, horizontalstart);
     }
 }
 
@@ -131,3 +190,16 @@ function drawBlock() {
         })
     })
 }
+
+document.addEventListener("keydown", event => {
+    if (event.keyCode === 39) {
+        right();
+        drawBlock()
+    } else if (event.keyCode === 37) {
+        left();
+        drawBlock();
+    } else if (event.keyCode === 40) {
+        drop();
+        drawBlock();
+    }
+})
